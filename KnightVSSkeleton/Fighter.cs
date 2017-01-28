bd09500a73bed599b7fb7e1ea43de4e121e31bda
Unknown <kossik6669@gmail.com>
2017-01-28 11:43:05 +0300

    начальная версия

---
+++ KnightVSSkeleton/Fighter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KnightVSSkeleton
+{
+    public class Fighter
+    {
+        PictureBox sprite;
+        private int health;
+
+        public Fighter(PictureBox sprite)
+        {
+            this.sprite = sprite;
+            this.health = 100;
+        }
+        public int MakeDamage()
+        {
+          Random random = new Random();
+        return random.Next(1, 10);
+        
+        }
+        public void ReceiveDemage(int howMachDemage)
+        {
+            health -= howMachDemage;
+            if (health <= 0)
+            {
+                Die();
+                health = 0;
+            }
+            
+        }
+        public int TellHealth()
+        {
+            return health;
+        }
+
+        private async void Die()
+        {
+            sprite.Image = Image.FromFile(@"C:\Users\Work\Desktop\KnightVSSkeleton-master\Assets/Skeleton_Death.gif");
+            await Task.Delay(900);
+            sprite.Enabled = false;
+        }
+
+        public bool isDead()
+        {
+            if (health <= 0) return true;
+            else return false;
+        }
+    }
+}
---
+++ KnightVSSkeleton/MainForm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KnightVSSkeleton
+{
+    public partial class MainForm : Form
+    {
+        Fighter Knight;
+        Fighter Skeleton;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            Knight = new Fighter(knightPictureBox);
+            Skeleton = new Fighter(skeletonPictureBox);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            skeletonPictureBox.Image.RotateFlip(RotateFlipType.RotateNoneFlipX);
+        }
+
+        /// <summary>
+        /// После того, как рыцарь и скелет проивзаимодействовали, нужно обновить их здоровье и картинки
+        /// </summary>
+        public void Update_All()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void skeletonAttacks_Click(object sender, EventArgs e)
+        {
+            Knight.ReceiveDemage(Skeleton.MakeDamage());
+            knightsHealth.Text = Knight.TellHealth().ToString();
+            if (Knight.isDead())
+            {
+                skeletonAttacks.Enabled = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Skeleton.ReceiveDemage(Knight.MakeDamage());
+            skeletonsHealth.Text = Skeleton.TellHealth().ToString();
+if (Skeleton.isDead())
+            {
+                button1.Enabled = false;
+            }
+        }
+
+        private void knightsHealth_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void skeletonsHealth_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
